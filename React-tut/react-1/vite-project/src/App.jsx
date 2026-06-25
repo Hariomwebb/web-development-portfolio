eb7e302d304381a6d09c4adcb1ea38b6812eff19
Hariom-BM <96159244+Hariom-BM@users.noreply.github.com>
2026-06-25 12:43:58 +0530

    react first tut done

---
+++ React-tut/react-1/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/header"
+
+const App=() => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
